06154d1b9dbf4d82e8ba4585389411416eaae2ac
KaueRonald <Kaueronald21.kr@gmail.com>
2024-04-08 23:44:23 -0300

    feat: create header and NavBar components

--- tri-dev/src/app/page.tsx
+++ tri-dev/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     <h1 style={{color: 'red'}}>hello world</h1>
+      <h1 style={{ color: 'red' }}>hello world</h1>
+      <Header />
     </main>
   );
 }
