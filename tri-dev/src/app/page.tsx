f1b7fb73d224512a9310bef0e1b59e7fde8a5f47
Luisfelippedev <felippe.luisdev@gmail.com>
2024-04-09 14:36:04 -0300

    feat: create QualitiesComponent

--- tri-dev/src/app/page.tsx
+++ tri-dev/src/app/page.tsx
@@ -2,12 +2,18 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import Header from "@/components/Header";
 import { Announcement } from "@/components/Announcement";
+import { Qualities } from "@/components/ Qualities";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Announcement />
       <Header />
+        <Box className={styles.backgroundQuality}>
+          <Qualities />
+        </Box>
+
     </main>
   );
 }
---
+++ tri-dev/src/components/ Qualities/index.tsx
@@ -0,0 +1,24 @@
+import styles from "./styles.module.scss";
+import { FaRegHandshake } from "react-icons/fa";
+import { GrSend } from "react-icons/gr";
+import { MdAccessTime } from "react-icons/md";
+import { Box } from "@mui/material";
+
+export const Qualities = () => {
+  return (
+    <Box className={styles.container}>
+      <Box className={styles.card}>
+        <FaRegHandshake size={30} className={styles.handsIcon} />
+        <p className={styles.textCard}>Atendimento Expresso</p>
+      </Box>
+      <Box className={styles.card}>
+        <GrSend size={30} className={styles.handsIcon} />
+        <p className={styles.textCard}>Qualidade e Agilidade</p>
+      </Box>
+      <Box className={styles.card} style={{ border: "none", marginRight: 0 }}>
+        <MdAccessTime size={30} className={styles.handsIcon} />
+        <p className={styles.textCard}>Prazo de 7 dias úteis</p>
+      </Box>
+    </Box>
+  );
+};
